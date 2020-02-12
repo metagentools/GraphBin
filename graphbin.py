582bcd52308f02b65d7b5a279f6e569df13af283
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2020-02-12 16:34:03 +1100

    Update graphbin code

--- graphbin.py
+++ graphbin.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 """graphbin.py: Improved binning of metagenomic contigs using SPAdes assembly graphs.
-
-GraphBin is a metagenomic contig binning tool that makes use of the contig 
-connectivity information from the assembly graph to bin contigs. It utilizes 
-the binning result of an existing binning tool and a label propagation algorithm 
-to correct mis-binned contigs and predict the labels of contigs which are 
-discarded due to short length.
 """
 
 import argparse
